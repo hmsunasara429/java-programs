38f896eeee2ec3259e3056eda3844e9c99b2d509
infomatica <infomatica@hasan>
2018-05-05 18:04:15 +0530

    Initial commit

---
+++ java-programs/src/main/java/com/hs/java/HSLinkedList.java
@@ -0,0 +1,13 @@
+package com.hs.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class HSLinkedList 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
